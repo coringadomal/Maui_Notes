ba3a9595aaf6cf1fb3d76dbcee16f9d0c3fc1b9b
coringadomal <catonholucas57@gmail.com>
2024-03-13 15:41:02 -0300

    Feat:Criei a pagina sobre

---
+++ projetinho/projetinho/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace projetinho
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ projetinho/projetinho/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace projetinho
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
